20b9bdb80f94d8ea1a18a7c48eadcc62c3a886a9
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-19 16:17:33 -0300

    Initial web

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/hello', () => {
+  return 'Hello Fastify...'
+})
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server running')
+})
