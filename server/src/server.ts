ee97094c5cd50168d7d70710a4c112a84b59f67a
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-20 22:48:39 -0300

    Create routes post habits and get day

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,12 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { prisma } from './lib/prisma'
+import { appRoute } from './lib/routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/hello', () => {
-  return 'Hello Fastify...'
-})
-
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany()
-  return habits
-})
+app.register(appRoute)
 
 app.listen({
   port: 3333,
