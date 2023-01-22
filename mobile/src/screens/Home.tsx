74f3876ba9d73b502634089447da4e992bde3ab0
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-22 13:14:56 -0300

    Fix server listen address. Integrate mobile with server

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,17 +1,53 @@
 import { useNavigation } from "@react-navigation/native";
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, Alert } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateDatesFromYear } from "../utils/generate-dates-from-year";
+import { api } from '../lib/axios'
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import dayjs from "dayjs";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateDatesFromYear();
 const minimumSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length;
 
+type SummaryProps = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number
+}>
+
 export function Home() {
+  const [loading, setLoading] = useState(true);
+  const [summary, setSummary] = useState<SummaryProps | null>(null);
 
   const { navigate } = useNavigation();
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const response = await api.get('summary');
+      setSummary(response.data);
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregar os dados.');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -29,31 +65,41 @@ export function Home() {
           ))
         }
       </View>
-      
-      <ScrollView 
+
+      <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
-        <View className="flex-row flex-wrap">
-          {
-            datesFromYearStart.map(date => (
-              <HabitDay
-                key={date.toISOString()}
-                onPress={() => navigate('habit', { date: date.toISOString() })}
-              />
-            ))
-          }
-          {
-            amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill })
-              .map((_, i) => (
-                <View
-                  key={i}
-                  className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
-                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
-                />
-              ))
-          }
-        </View>
+        { summary &&
+          <View className="flex-row flex-wrap">
+            {
+              datesFromYearStart.map(date => {
+                const dayWithHabits = summary.find(day => {
+                  return dayjs(date).isSame(day.date, 'day');
+                })
+                return (
+                  <HabitDay
+                    key={date.toISOString()}
+                    date={date}
+                    amountOfHabits={dayWithHabits?.amount}
+                    amountCompleted={dayWithHabits?.completed}
+                    onPress={() => navigate('habit', { date: date.toISOString() })}
+                  />
+                )
+              })
+            }
+            {
+              amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill })
+                .map((_, i) => (
+                  <View
+                    key={i}
+                    className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
+                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                  />
+                ))
+            }
+          </View>
+        }
       </ScrollView>
 
     </View>
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,7 @@ app.register(appRoute)
 
 app.listen({
   port: 3333,
+  host: '0.0.0.0',
 }).then(() => {
-  console.log('HTTP Server running')
+  console.log(`HTTP Server running on port 3333`)
 })
